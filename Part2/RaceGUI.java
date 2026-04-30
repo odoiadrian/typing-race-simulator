6d935134df2b6023d222cda199889a99436ce092
odoiadrian <odoiadrian07@gmail.com>
2026-04-30 13:21:36 +0100

    Created new JTextArea object in main method that users can't type in

--- Part2/RaceGUI.java
+++ Part2/RaceGUI.java
@@ -1,8 +1,10 @@
-import javax.swing.JFrame; 
+import javax.swing.*; 
 public class RaceGUI{
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Typing Race Simulator");
 
+        JTextArea display = new JTextArea();
+        display.setEditable(false);
     }
 }
