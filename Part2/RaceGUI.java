6f351a936636f106cb3efae97a2a5355c89d2b21
odoiadrian <odoiadrian07@gmail.com>
2026-04-30 13:39:05 +0100

    Undid name change of the class

--- Part2/RaceGUI.java
+++ Part2/RaceGUI.java
@@ -1,5 +1,5 @@
 import javax.swing.*; 
-public class startRaceGUI{
+public class RaceGUI{
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Typing Race Simulator");
